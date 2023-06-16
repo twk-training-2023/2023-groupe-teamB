953e70bd3f0d5ca6a5328c44b7d24e28da599c65
hayashi03 <ryutaro-hayashi@twk.co.jp>
2023-06-16 15:13:48 +0900

    create project

---
+++ TeamBProject/WEB-INF/src/sample/Calc.java
@@ -0,0 +1,58 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calc
+ */
+@WebServlet("/Calc")
+public class Calc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calc() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		int num1;
+		//リクエストデータの取得
+		if (null == request.getParameter("num1") || "".equals(request.getParameter("num1"))) {
+			num1 = 10;
+		} else {
+			num1 = Integer.parseInt(request.getParameter("num1"));
+		}
+		//100倍にする。
+		num1 = num1 * 100;
+		//リクエストオブジェクトへの設定
+		request.setAttribute("num", num1);
+		//JSPへのフォワード処理
+		RequestDispatcher rd = request.getRequestDispatcher("/view/out.jsp");
+		rd.forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ TeamBProject/view/in.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>POST送信データを扱うサーブレット</title>
+<head>
+<body>
+	<div style="text-align: center;">
+		<h1>HelloWorld</h1>
+		<p>下記の値を100倍にして返します。
+		<form method"get" name=form1 action="/TeamBProject/Calc">
+			<input type"text" name="num1"> <input type="submit"
+				value="送信">
+		</form>
+	</div>
+</body>
+</html>
